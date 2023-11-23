82209a15e9cc0c4c102641db4ef56e89adb8d26a
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-23 13:15:45 +0100

    Added barrels files

---
+++ src/components/index.ts
@@ -0,0 +1,34 @@
+import { BouncingBalls } from "./BouncingBalls";
+import { RadarChart } from "./Charts";
+import {
+  Anchor,
+  ChartContainer,
+  ChartTitle,
+  ListElement,
+  NameButtons,
+  StatsAndChart,
+  StatsContainer,
+} from "./Containers";
+import { SearchInput } from "./SearchInput";
+import { Stats } from "./Stats";
+import { Fire } from "./icons/Icons";
+
+export {
+  /* FUN */
+  BouncingBalls,
+  /* CHARTS */
+  RadarChart,
+  /* CONTAINERS */
+  ChartContainer,
+  ChartTitle,
+  StatsAndChart,
+  StatsContainer,
+  Stats,
+  ListElement,
+  /* UTILS */
+  Anchor,
+  NameButtons,
+  SearchInput,
+  /* ICONS */
+  Fire,
+};
