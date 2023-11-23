255e6bdf5959ae9944092b5686bc2a8565523f8f
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-23 13:54:21 +0100

    Add new  integrations

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
+import { Fragment, useEffect, useState, useRef } from "react";
 import creatures from "@Data";
-import { RadarChart } from "./components/Charts";
-import { Fragment, useState } from "react";
-import { DataType, HeatAllDataType, OptionalAllDataType } from "./types";
-import { beautifyK, empty, getGenericStats, noNumbers } from "./components/helpers";
+import { cleanData, getAllRandomColors, getElementsAroundIndex, debounce } from "@Helpers";
 import { ChartData } from "chart.js";
 import {
+  RadarChart,
   ListElement,
-  NameButton,
+  NameButtons,
   StatsAndChart,
   ChartContainer,
   StatsContainer,
-} from "./components/Containers";
-import { Stats } from "./components/Stats";
+  Anchor,
+  Stats,
+  SearchInput,
+} from "@Components";
 
-const LIMITER = 10;
+console.log("START");
+
+const LIMITER = 50;
+const boolArr = new Array(LIMITER).fill(false);
 
 const App = () => {
-  const [openRadar, setOpenRadar] = useState(new Array(LIMITER).fill(false));
-  const [openStats, setOpenStats] = useState(new Array(LIMITER).fill(false));
-  const { data } = creatures;
-  const { finalData, finalChart } = cleanData(data.slice(0, LIMITER));
+  console.time("App");
+  const [scroll, setScroll] = useState(0);
+
+  const [search, setSearch] = useState("");
+  const [data] = useState(cleanData(creatures.data.slice(0, LIMITER)));
+  const [colors] = useState(getAllRandomColors(LIMITER));
+  const [openRadar, setOpenRadar] = useState(boolArr);
+  const [openStats, setOpenStats] = useState(boolArr);
+  const listRef = useRef<HTMLLIElement | null>(null);
 
   const handleOpenRadar = (i: number) => {
     setOpenRadar((prev) => {
@@ -37,156 +46,83 @@ const App = () => {
     });
   };
 
+  const debouncedSearch = debounce(setSearch, 100);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedSearch(e.currentTarget.value);
+  };
+
+  const finalData = data.finalData.filter((creature) =>
+    creature.name.toLowerCase().includes(search)
+  );
+
+  const handleScroll = debounce(() => {
+    setScroll(window.scrollY);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const viewIndex = Math.floor((scroll * 1.15) / window.innerHeight);
+  const finalInViewElement = getElementsAroundIndex(finalData, viewIndex, 10);
+  console.log(listRef.current);
+
+  console.timeEnd("App");
+
   return (
     <>
-      <h1>Mythical Creatures</h1>
+      <SearchInput handleSearch={handleSearch} search={search} />
       <ul>
-        {finalData.map((creature, i) => {
+        {/* LOOP */}
+        {finalInViewElement.map((creature, i) => {
           const newData = Object.entries(creature);
           const name = newData[0][1]; // value
           const rest = newData.slice(1);
           return (
             <ListElement key={i}>
-              <NameButton
+              <span ref={listRef} id="ref" />
+              <NameButtons
                 name={name}
+                color={colors[i]}
                 handleOpenRadar={() => handleOpenRadar(i)}
                 handleOpenStats={() => handleOpenStats(i)}
               >
                 <>Reveal stats</>
                 <>Reveal radar</>
-              </NameButton>
+              </NameButtons>
               <StatsAndChart>
                 <StatsContainer>
                   {rest.map(([label, value]) => {
-                    return value === "" || finalChart[i].labels.includes(label) ? (
+                    return value === "" || data.finalChart[i].labels.includes(label) ? (
                       <Fragment key={label} />
                     ) : (
-                      <Stats label={label} value={value} key={label} open={openStats[i]} />
+                      <Stats
+                        label={label}
+                        value={value}
+                        key={label}
+                        open={openStats[i]}
+                        color={colors[i]}
+                      />
                     );
                   })}
                 </StatsContainer>
                 <ChartContainer isOpen={openRadar[i]}>
-                  <RadarChart data={finalChart[i] as ChartData<"radar">} />
+                  <RadarChart data={data.finalChart[i] as ChartData<"radar">} color={colors[i]} />
                 </ChartContainer>
               </StatsAndChart>
             </ListElement>
           );
         })}
       </ul>
+      {/* <BouncingBalls /> */}
+      <Anchor />
     </>
   );
 };
 
 export default App;
-
-const cleanData = (data: DataType[]) => {
-  const t1 = performance.now();
-
-  const map: Map<string, string> = new Map();
-  let cleanedData: Record<string, string> = {};
-  const strMap: Set<string> = new Set();
-  const nbrsMap: Set<string> = new Set();
-
-  const heatNbrsMap: Map<string, number> = new Map();
-  const heatNbrsObj: HeatAllDataType = {} as HeatAllDataType;
-
-  const finalData: Record<string, string>[] = [];
-  const nullable: Map<string, string> = new Map();
-
-  const allKeys: Set<string> = new Set();
-  const allKeysEmpty: OptionalAllDataType = {};
-
-  let labels = [];
-  let chartData = [];
-  const finalChart = [];
-
-  for (let i = 0; i < data.length; i++) {
-    //
-
-    for (let [key, value] of Object.entries(data[i])) {
-      key = beautifyK(key) as keyof OptionalAllDataType;
-
-      //
-
-      if (map.has(key)) {
-        value += ", " + map.get(key);
-      }
-
-      //FINAL DATA
-      //--
-
-      map.set(key, value);
-      value = value.replace(/,\s,/g, "").replace(/\s,\s/g, "");
-      value = value.toLowerCase().trim();
-      value = value.charAt(0).toUpperCase() + value.slice(1);
-
-      cleanedData[key] = value;
-
-      //NULLABLE // ALL ENTRIES ARE EMPTY AT SOME POINT
-      //--
-      if (empty(value)) {
-        nullable.set(key, value);
-      }
-
-      //ALL KEYS
-      //--
-      allKeys.add(key);
-
-      //ONLY NUMBERS // 20 are only strings
-      //ORDERED BY MOST OCCURRING OBJ & MAP
-      const n = parseInt(value);
-      if (noNumbers(value) === "" && !isNaN(n)) {
-        nbrsMap.add(key);
-        const current = heatNbrsMap.get(key);
-        heatNbrsMap.set(key, current ? current + 1 : 1);
-        heatNbrsObj[key] = current ? current + 1 : 1;
-
-        // radarLabelsAndDataElements.push(getGenericStats(key, n));
-        const [lbl, val] = getGenericStats(key, n);
-        labels.push(...lbl);
-        chartData.push(...val);
-      }
-    }
-
-    // RESET FOR NEXT ITERATION
-    //--
-    finalData.push(cleanedData);
-    finalChart.push({ labels, datasets: [{ data: chartData }] });
-    map.clear();
-    cleanedData = {};
-    labels = [];
-    chartData = [];
-  }
-
-  const heatOnlyNbrs = Array.from(heatNbrsMap, ([, value]) => value)
-    .flat()
-    .sort((a, b) => b - a);
-
-  const sortedHeat = Object.entries(heatNbrsObj)
-    .sort(([, a], [, b]) => b - a)
-    .flat()
-    .filter((_, i) => i % 2 === 0);
-
-  allKeys.forEach((key) => {
-    allKeysEmpty[key] = "";
-  });
-
-  // FUNCTION TIME EXECUTION
-  console.log("cleanData took " + (performance.now() - t1) + " milliseconds.");
-
-  return {
-    finalData,
-    nullable,
-    nbrsMap,
-    allKeys,
-    heatOnlyNbrs,
-    allKeysEmpty,
-    strMap,
-    heatNbrsObj,
-    sortedHeat,
-    finalChart,
-  };
-};
 
 // SORTED BY MOST OCCURRING
 // ['perception', 2288]
