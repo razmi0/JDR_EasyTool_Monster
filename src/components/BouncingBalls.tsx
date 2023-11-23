e7cc2193a70d144ca47527e777cc76829cc65296
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-23 13:04:20 +0100

    Add BouncingBalls & refacto NameButtons & Anchor

---
+++ src/components/BouncingBalls.tsx
@@ -0,0 +1,85 @@
+import { getRandomColor } from "./helpers";
+
+export const BouncingBalls = () => {
+  const canvas = {
+    element: document.getElementById("canvas") as BallType,
+    width: 800, //600,
+    height: 600, //400,
+    initialize: function () {
+      this.element.style.width = this.width + "px";
+      this.element.style.height = this.height + "px";
+
+      document.body.appendChild(this.element);
+    },
+  };
+
+  const colors = new Array(50).map(() => getRandomColor());
+  canvas.element.addEventListener("click", function (e) {
+    const xCoordinate = e.x - (window.innerWidth - canvas.width) / 2;
+    const yCoordinate = e.y - (window.innerHeight - canvas.height) / 2;
+    const randomNumber = Math.floor(Math.random() * 3);
+    const selectedColor = colors[randomNumber];
+
+    const dx = Math.floor(Math.random() * 10);
+    const dy = Math.floor(Math.random() * 10);
+
+    Ball.create(selectedColor, dx, dy).draw(xCoordinate, yCoordinate);
+  });
+  interface BallType extends HTMLElement {
+    dx: number;
+    dy: number;
+    width: number;
+    height: number;
+    element: HTMLElement;
+    moveTo: (x: number, y: number) => void;
+    changeDirectionIfNecessary: (x: number, y: number) => void;
+    draw: (x: number, y: number) => void;
+  }
+  const Ball = {
+    create: function (color: string, dx: number, dy: number) {
+      const newBall = Object.create(this);
+      newBall.dx = dx;
+      newBall.dy = dy;
+      newBall.width = 40;
+      newBall.height = 40;
+      newBall.element = document.createElement("div") as HTMLElement;
+      newBall.element.style.backgroundColor = color;
+      newBall.element.style.width = newBall.width + "px";
+      newBall.element.style.height = newBall.height + "px";
+      newBall.element.className += " ball";
+      newBall.width = parseInt(newBall.element.style.width);
+      newBall.height = parseInt(newBall.element.style.height);
+      canvas.element.appendChild(newBall.element);
+      return newBall as BallType;
+    },
+    moveTo: function (this: BallType, x: number, y: number) {
+      this.element.style.left = (x + "px") as string;
+      this.element.style.top = (y + "px") as string;
+    },
+    changeDirectionIfNecessary: function (this: BallType, x: number, y: number) {
+      if (x < 0 || x > canvas.width - this.width) {
+        this.dx = -this.dx;
+      }
+      if (y < 0 || y > canvas.height - this.height) {
+        this.dy = -this.dy;
+      }
+    },
+    draw: function (this: BallType, x: number, y: number) {
+      this.moveTo(x, y);
+      const ball = this as BallType;
+      setTimeout(function (this: BallType) {
+        ball.changeDirectionIfNecessary(x, y);
+        ball.draw(x + ball.dx, y + ball.dy);
+      }, 1000 / 60);
+    },
+  };
+
+  canvas.initialize();
+  return (
+    <div style={{ height: "100px" }}>
+      <h1>Bouncing Balls</h1>
+      <p>Click inside the ball area to add balls</p>
+      <div id="canvas"></div>
+    </div>
+  );
+};
--- src/components/Containers.tsx
+++ src/components/Containers.tsx
@@ -1,30 +1,73 @@
-import { ReactNode } from "react";
+import { ReactNode, useId } from "react";
 import styles from "./_.module.css";
+import { scrollToTop } from "./helpers";
+import { Fire } from "@/icons/Icons";
 
 type Container = { children: ReactNode };
+
 export const ListElement = ({ children }: Container) => {
   return <li className={styles["listElementStyle"]}>{children}</li>;
 };
 
 interface NameProps {
   name: string;
+  color: string;
   handleOpenRadar: () => void;
   handleOpenStats: () => void;
   children: ReactNode[];
 }
-export const NameButton = ({ name, handleOpenRadar, handleOpenStats, children }: NameProps) => {
+export const NameButtons = ({
+  name,
+  color,
+  handleOpenRadar,
+  handleOpenStats,
+  children,
+}: NameProps) => {
+  const id = useId().replace(/:/g, "_") + "_button";
+
   return (
-    <div id="name" className={styles["name-ctn"]}>
-      <span className={styles["name-style"]}> {name} </span>
-      <button className={styles["button-style"]} onClick={handleOpenStats}>
+    <div id="name" className={styles["name-ctn"] + " " + id}>
+      <style>
+        {`
+
+              .${id}:hover {
+                border-color: ${color};
+                color: ${color};
+                transition : 0.3s ease-in;
+              }
+              
+      `}
+      </style>
+      <Fire />
+      <span className={styles["name-style"]} style={{ color: color }}>
+        {name}
+      </span>
+      <button className={styles["button-style"] + " " + id} onClick={handleOpenStats}>
         {children[0]}
       </button>
-      <button className={styles["button-style"]} onClick={handleOpenRadar}>
+      <button
+        data-btn-color={id}
+        className={styles["button-style"] + " " + id}
+        onClick={handleOpenRadar}
+      >
         {children[1]}
       </button>
     </div>
   );
 };
+
+// button {
+//   border-radius: 8px;
+//   border: 1px solid transparent;
+//   padding: 0.6em 1.2em;
+//   font-size: 1em;
+//   font-weight: 500;
+//   font-family: inherit;
+//   background-color: #1a1a1a;
+//   cursor: pointer;
+//   transition: border-color 0.25s;
+//   color: #fff;
+// }
 
 export const StatsAndChart = ({ children }: Container) => {
   return (
@@ -57,4 +100,26 @@ export const ChartContainer = ({ children, isOpen }: ChartProps) => {
 
 export const ChartTitle = ({ children }: Container) => {
   return <span className={styles["chart-title"]}>{children}</span>;
+};
+
+export const Anchor = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        right: "0",
+        top: "0",
+        padding: "7px",
+        backgroundColor: "grey",
+        color: "white",
+        borderRadius: "50%",
+        margin: "5%",
+        fontSize: ".8em",
+      }}
+    >
+      <a href="#" onClick={scrollToTop}>
+        Top
+      </a>
+    </div>
+  );
 };
